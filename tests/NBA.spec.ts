7dcf094ceff07645e52c8c69ec8f9893344cc6e9
dlee-dev <david.y.lee.dev@gmail.com>
2023-11-09 17:11:53 -0500

    First tests

---
+++ tests/NBA.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.nba.com/');
+  await page.getByRole('link', { name: 'Standings' }).click();
+  await page.getByRole('link', { name: '1 Logo Philadelphia 76ers' }).click();
+  await page.getByRole('link', { name: 'Joel Embiid' }).click();
+  await page.getByText('Philadelphia 76ers | #21 | Center-Forward').click();
+  await page.getByText('Philadelphia 76ers | #21 | Center-Forward').click();
+  await page.getByText('Joel', { exact: true }).click();
+  await page.getByText('Embiid', { exact: true }).click();
+  await page.getByRole('img', { name: 'Joel Embiid Headshot' }).click();
+});
