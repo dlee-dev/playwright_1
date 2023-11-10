03fb12b1d515a5e728129b8c33b7beb37961e274
dlee-dev <david.y.lee.dev@gmail.com>
2023-11-10 18:18:56 -0500

    Fixed NBA test

--- tests/NBA.spec.js
+++ tests/NBA.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://www.nba.com/');
   await page.getByRole('link', { name: 'Standings' }).click();
-  await page.getByRole('link', { name: '1 Logo Philadelphia 76ers' }).click();
+  await page.getByRole('link', { name: 'Philadelphia 76ers' }).click();
   await page.getByRole('link', { name: 'Joel Embiid' }).click();
   await page.getByText('Philadelphia 76ers | #21 | Center-Forward').click();
   await page.getByText('Philadelphia 76ers | #21 | Center-Forward').click();
